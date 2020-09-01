dcf07a54bd3c010eaff107bd7e27bc35c97358ce
Maia <maia.mchedlishvili@gmail.com>
2020-09-01 18:08:23 +0400

    add transaction state

---
+++ Solution/TbcBank.EcommerceClient/CodesAndStatuses/TransactionState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TbcBank.EcommerceClient
+{
+    public enum TransactionState
+    {
+        Unknown = 0,
+        Pending = 1,
+        Succeeded = 2,
+        Failed = 3
+    }
+}
--- Solution/TbcBank.EcommerceClient/Models/Operation/CheckTransactionResult.cs
+++ Solution/TbcBank.EcommerceClient/Models/Operation/CheckTransactionResult.cs
@@ -13,6 +13,7 @@ namespace TbcBank.EcommerceClient
         public string CardNumber { get; set; }
         public string ReocurringPaymentBillerClientId { get; set; }
         public DateTimeOffset? ReocurringPaymentExpiry { get; set; }
+        public TransactionState State { get; set; }
 
         public CheckTransactionResult(HttpRequestResult httpResult)
             : base(httpResult)
@@ -30,7 +31,36 @@ namespace TbcBank.EcommerceClient
             ReocurringPaymentBillerClientId = GetResponseKeyValue("RECC_PMNT_ID");
 
             ParseReccPmntExpiry();
+            State = ParseState();
         }
+
+        private TransactionState ParseState()
+        {
+            switch (Result)
+            {
+                case TransactionResultStatus.CREATED:
+                case TransactionResultStatus.PENDING:
+                case TransactionResultStatus.ACTIVE:
+                    return TransactionState.Pending;
+
+                case TransactionResultStatus.OK:
+                case TransactionResultStatus.FINISHED:
+                    return TransactionState.Succeeded;
+
+                case TransactionResultStatus.FAILED:
+                case TransactionResultStatus.DECLINED:
+                case TransactionResultStatus.REVERSED:
+                case TransactionResultStatus.AUTOREVERSED:
+                case TransactionResultStatus.TIMEOUT:
+                case TransactionResultStatus.RETURNED:
+                case TransactionResultStatus.CANCELLED:
+                    return TransactionState.Failed;
+
+                default:
+                    return TransactionState.Unknown;
+            }
+        }
+
         protected override bool IsFinancialOperationSuccessful()
         {
             return ResultCode?.StartsWith("0") == true;
