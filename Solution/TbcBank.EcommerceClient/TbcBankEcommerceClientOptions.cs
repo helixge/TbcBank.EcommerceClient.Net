2f27b10ff386533ea4269ea61b13b51c89152b15
To Ka <me@toka.ge>
2020-12-27 01:23:08 +0400

    Add currencies and merchant id to options

--- Solution/TbcBank.EcommerceClient/TbcBankEcommerceClientOptions.cs
+++ Solution/TbcBank.EcommerceClient/TbcBankEcommerceClientOptions.cs
@@ -7,6 +7,10 @@ namespace TbcBank.EcommerceClient
     public class TbcBankEcommerceClientOptions
     {
         /// <summary>
+        /// Merchant ID
+        /// </summary>
+        public string MerchantId { get; set; }
+        /// <summary>
         /// Certificate file content
         /// Either CertData or CertPath should be specified. Do not specify both
         /// </summary>
@@ -24,18 +28,25 @@ namespace TbcBank.EcommerceClient
         /// TBC Envirionment
         /// </summary>
         public TbcEnvironment Environment { get; set; }
+        /// <summary>
+        /// Merchant available currencies
+        /// </summary>
+        public CurrencyCode[] Currencies { get; set; }
 
         public void Validate()
         {
             if (CertData != null && !String.IsNullOrWhiteSpace(CertPath))
-            {
                 throw new Exception("Invalid options, both CertData and CertPath are assigned. Only one of them can be assigned");
-            }
 
             if (CertData == null && String.IsNullOrWhiteSpace(CertPath))
-            {
                 throw new Exception("Invalid options, neither CertData not CertPath are assigned. One of them should be assigned.");
-            }
+
+            if (String.IsNullOrWhiteSpace(MerchantId))
+                throw new Exception("Invalid options, merchant ID is not defined");
+
+            if (!(Currencies?.Length > 0))
+                throw new Exception("Invalid options, no currencies have been defined");
+
         }
     }
 }
